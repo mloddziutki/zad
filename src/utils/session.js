43529298ee60762b80fc9f6f023c3baafe104ee8
mloddziutki <123062429+mloddziutki@users.noreply.github.com>
2023-01-20 10:03:02 +0100

    Add files via upload

---
+++ src/utils/session.js
@@ -0,0 +1,13 @@
+export const setUserSession = (token, user) => {
+    localStorage.setItem('token', token)
+  }
+  
+  export const getToken = () => {
+    if(localStorage.getItem('token') !== null) return true;
+    return false;
+  }
+  
+  export const removeUserSession = () => {
+    localStorage.removeItem('token');
+  }
+  
